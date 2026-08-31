a169475e882d0bbac731a91834e8d329e4f50e43
RavemanThc <vlad.garkusha1999@gmail.com>
2026-08-31 22:09:06 +0300

    change Teacher client error

--- README.md
+++
@@ -1,2 +0,0 @@
-# EnglishHome
-EnglishHome A web platform connecting independent English teachers with students for online lessons and educational services.
--- app/teachers/TeachersClient.tsx
+++ app/teachers/TeachersClient.tsx
@@ -3,17 +3,11 @@
 import { useState } from "react";
 import Filters from "@/components/Filters/Filters";
 import TeachersList from "@/components/Teachers/TeachersList";
-import { Teacher } from "@/types/teachers";
+import { FiltersState, Teacher } from "@/types/teachers";
 import css from "./TeachersPage.module.css";
 
 interface Props {
   teachers: Teacher[];
-}
-
-interface FiltersState {
-  language: string | null;
-  level: string | null;
-  price: number | null;
 }
 
 const TeachersClient = ({ teachers }: Props) => {
@@ -21,29 +15,14 @@ const TeachersClient = ({ teachers }: Props) => {
     language: null,
     level: null,
     price: null,
-  });
-
-  const filteredTeachers = teachers.filter((teacher) => {
-    const languageMatch =
-      !filters.language || teacher.languages.includes(filters.language);
-
-    const levelMatch = !filters.level || teacher.levels.includes(filters.level);
-
-    const priceMatch =
-      filters.price === null || teacher.price_per_hour === filters.price;
-
-    return languageMatch && levelMatch && priceMatch;
+    favoritesOnly: false,
   });
 
   return (
     <section className={css.TeachersPage}>
-      <Filters
-        teachers={teachers}
-        filters={filters}
-        onFilterChange={setFilters}
-      />
+      <Filters teachers={teachers} onFilterChange={setFilters} />
 
-      <TeachersList teachers={filteredTeachers} filters={filters} />
+      <TeachersList teachers={teachers} filters={filters} />
     </section>
   );
 };
